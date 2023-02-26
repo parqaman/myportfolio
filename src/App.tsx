8556e081288996210d3e63fdf400e87ab0726cd2
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2023-02-26 19:16:22 +0100

    removed browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { AppRoutes } from '../AppRoutes'
 import { HomePage } from "./pages/HomePage"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage/>} />
         </Routes>
-      </BrowserRouter>
     </div>
   )
 }
