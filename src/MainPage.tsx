250267eaf7f2bf7d08abf4eac71feb9f47b9c3ac
Farouq Abdurrahman <farouq03@gmail.com>
2023-10-23 17:48:12 +0200

    implement framer motion in aboutme

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -3,16 +3,8 @@ import { HomeSection } from "./components/HomeSection";
 import { AboutMe } from "./components/AboutMe";
 import { TechStack } from "./components/TechStack";
 import { Contact } from "./components/Contact";
-import { useEffect, useRef } from "react";
-import { useScroll, motion, useTransform, useMotionValue } from "framer-motion";
 
 export const MainPage = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "start start"],
-  });
-
   return (
     <AppLayout>
       <HomeSection />
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,20 +1,40 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 export const AboutMe = () => {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: aboutRef,
+    offset: ["start center", "end end"],
+  });
+
   return (
-    <div id='about' className="min-h-screen flex  flex-col items-center justify-center">
-        <div className="content opacity-0 transition-all duration-700 max-w-3xl flex flex-col gap-5">
-            <h1 className='text-4xl font-semibold'>About me.</h1>
-            <p className='text-xl'>
-                Ever since I was young, I have been fascinated by computers and technology.
-                As I grew older, I became increasingly interested in programming and software development.
-                This led me to pursue a degree in computer science.
-            </p>
-            <p className='text-xl'>
-                I am currently in my final year at <strong>Darmstadt University of Applied Sciences</strong>.
-                During one of my semester, I had the opportunity to take two elective modules, 
-                namely <strong>Data Warehouse Technologies</strong> and <strong>Advanced Web Development</strong>,
-                which provided me with hands-on experience and exposure to the latest technologies in these fields. 
-            </p>
-        </div>
-    </div>
-    )
-}
+    <motion.div
+      ref={aboutRef}
+      style={{
+        opacity: useTransform(scrollYProgress, [0, 1], [0, 1]),
+      }}
+      id="about"
+      className="h-screen flex flex-col items-center justify-center text-justify"
+    >
+      <div className="transition-all duration-700 max-w-3xl flex flex-col gap-5">
+        <h1 className="text-4xl font-semibold">About me.</h1>
+        <p className="text-xl">
+          Ever since I was young, I have been fascinated by computers and
+          technology. As I grew older, I became interested in programming and
+          software development. This led me to pursue a degree in computer
+          science.
+        </p>
+        <p className="text-xl">
+          I am currently in my final year at{" "}
+          <strong>Darmstadt University of Applied Sciences</strong>. During one
+          of my semester, I had the opportunity to take two elective modules,
+          namely <strong>Data Warehouse Technologies</strong> and{" "}
+          <strong>Advanced Web Development</strong>, which provided me with
+          hands-on experience and exposure to the latest technologies in these
+          fields.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
